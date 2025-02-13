740c631f9bc0ec688be0a83642cd30b58c07414c
Pedro Gomes <79362098+pedrolucas201@users.noreply.github.com>
2025-02-13 18:36:37 -0300

    feat: allow CORS to backend host

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ["https://task-manager-zic5.onrender.com"]
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # Frontend rodando com Vite
     "http://127.0.0.1:5173",
+    "https://task-manager-zic5.onrender.com",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
