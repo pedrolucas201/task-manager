c74589c93a3573cd70ae5a00539b21f6341b3c75
Pedro Gomes <79362098+pedrolucas201@users.noreply.github.com>
2025-02-13 18:41:48 -0300

    feat: allow CORS to backend host

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import dj_database_url
+from corsheaders.defaults import default_headers
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
